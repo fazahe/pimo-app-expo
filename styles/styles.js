065a40df1fb6db686373971d9e37c0e7458225a2
Fabián Creativo <fabiancreativo@hotmail.com>
2024-07-29 12:39:30 -0500

    creación de estilos y variables

---
+++ styles/styles.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+const globalStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FCFCF7',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 18,
+    color: 'red',
+    fontWeight: 'bold',
+  },
+  // Añade más estilos según sea necesario
+});
+
+export default globalStyles;
---
+++ styles/variables.js
@@ -0,0 +1,18 @@
+export const colors = {
+    primary: '#E75553',
+    secondary: '#2ecc71',
+    background: '#ecf0f1',
+    title: '#1E2130',
+    text: '#4D4D4D',
+  };
+  
+  export const spacing = {
+    small: 8,
+    medium: 16,
+    large: 24,
+  };
+  
+  export const fonts = {
+    regular: 16,
+    large: 20,
+  };
